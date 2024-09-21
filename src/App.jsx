0a96cfd433b952dc9e52e506e738f2fe3fe56717
Aditya Narayan Sahoo <adityasahoo246@gmail.com>
2024-09-21 12:32:13 +0530

    Add Todo Component and Improve Styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,12 @@
+import Todos from "./components/Todos";
+import AddTodo from "./components/AddTodo";
 const App = () => {
-  return <div></div>;
+  return (
+    <div>
+      <AddTodo />
+      <Todos />
+    </div>
+  );
 };
 
 export default App;
--- src/components/AddTodo.jsx
+++ src/components/AddTodo.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { useDispatch } from "react-redux";
 import { addTodo } from "../features/todoSlice";
+
 const AddTodo = () => {
   const [input, setInput] = useState("");
   const dispatch = useDispatch();
@@ -9,21 +10,23 @@ const AddTodo = () => {
     <form
       onSubmit={(e) => {
         e.preventDefault();
-        dispatch(addTodo(input));
-        setInput("");
+        if (input.trim()) {
+          dispatch(addTodo(input));
+          setInput("");
+        }
       }}
-      className="space-x-3 mt-12"
+      className="flex justify-center mt-12 space-x-3 mb-4"
     >
       <input
         type="text"
-        className="bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+        className="bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-2 px-4 leading-tight transition-colors duration-200 ease-in-out shadow-sm"
         placeholder="Enter a Todo..."
         value={input}
         onChange={(e) => setInput(e.target.value)}
       />
       <button
         type="submit"
-        className="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg"
+        className="text-white bg-indigo-600 border border-indigo-500 py-2 px-6 focus:outline-none hover:bg-indigo-700 rounded-lg transition-colors duration-200 shadow-md"
       >
         Add Todo
       </button>
